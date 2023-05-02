b001ba216f1af9fe021d5f09eca17ce2d041c77c
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2023-05-02 18:40:19 -0500

    Seed for restaurants and user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Reservation.destroy_all
+Restaurant.destroy_all
+User.destroy_all
+puts 'Deleting records from the DB...'
+
+user = User.new(email: 'example@email.com', password: '123456', password_confirmation: '123456')
+user.save!
+
+(1..10).each do |num|
+  restaurant = Restaurant.new
+  restaurant.user = user
+  restaurant.name = "Testing name#{num}"
+  restaurant.description = "Testing description#{num}"
+  restaurant.address = "Testing address#{num}"
+  restaurant.city = "Testing city#{num}"
+
+  restaurant.photo.attach(io: URI.open('https://res.cloudinary.com/dpcj8gyeu/image/upload/v1683052000/dining-restaurants-1057448_nvu8j8.jpg'),
+                          filename: 'restaurant.png',
+                          content_type: 'image/png')
+
+  restaurant.save!
+end
