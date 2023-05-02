e7301d3773ca5786a1dcd8550dbe9fe5cec9e8df
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2023-05-02 18:45:02 -0500

    Auth before action suspend for testing.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+ # skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :set_restaurants
 
   def home
+    @reservation = Reservation.new
+  end
+
+  private
+
+  def set_restaurants
+    @restaurants = Restaurant.all
   end
 end
