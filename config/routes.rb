d05f6b87cd6ebc7ba88ad3c3b294bd933abea06f
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2023-05-03 01:47:05 -0500

    New routes for CRUD of a restaurant.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :restaurants do
+  resources :restaurants, except: :show do
     resources :reservations, only: %i[new create]
   end
 
