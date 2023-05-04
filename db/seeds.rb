c9d0354cf2f5250abc3839ed7fa0dd7bfb190a7c
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2023-05-03 20:12:53 -0500

    Name and hoster attributes added to user model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,28 @@ Restaurant.destroy_all
 User.destroy_all
 puts 'Deleting records from the DB...'
 
-user = User.new(email: 'example@email.com', password: '123456', password_confirmation: '123456')
-user.save!
+User.create(name: 'Client', email: 'client@email.com', password: '123456', password_confirmation: '123456', hoster: false)
+hoster1 = User.create(name: 'Hoster1', email: 'hoster1@email.com', password: '123456', password_confirmation: '123456', hoster: true)
+hoster2 = User.create(name: 'Hoster2', email: 'hoster2@email.com', password: '123456', password_confirmation: '123456', hoster: true)
 
-(1..10).each do |num|
+(1..3).each do |num|
   restaurant = Restaurant.new
-  restaurant.user = user
+  restaurant.user = hoster1
+  restaurant.name = "Testing name#{num}"
+  restaurant.description = "Testing description#{num}"
+  restaurant.address = "Testing address#{num}"
+  restaurant.city = "Testing city#{num}"
+
+  restaurant.photo.attach(io: URI.open('https://res.cloudinary.com/dpcj8gyeu/image/upload/v1683052000/dining-restaurants-1057448_nvu8j8.jpg'),
+                          filename: 'restaurant.png',
+                          content_type: 'image/png')
+
+  restaurant.save!
+end
+
+(4..6).each do |num|
+  restaurant = Restaurant.new
+  restaurant.user = hoster2
   restaurant.name = "Testing name#{num}"
   restaurant.description = "Testing description#{num}"
   restaurant.address = "Testing address#{num}"
